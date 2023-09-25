ba82812240d88361a667dca93189ecdaca3f7891
Monika.Strzalka <monika.strzalka@columbusglobal.com>
2023-09-25 12:14:25 +0000

    Merged in bugfix/WGP2023-112-pagination-styling-reset-filters-button-scroll-button-on-modal-fix (pull request #88)
    Bugfix/WGP2023-112 pagination styling reset filters button scroll button on modal fix
    
    * WGP2023-112 Pagination styling fix, reset filters funtionality, scroll on mobile.
    
    * Merge branch 'develop' into bugfix/WGP2023-112-pagination-styling-reset-filters-button-scroll-button-on-modal-fix
    
    * WGP2023-112 Fix to reset filters on mobile.
    
    * WGP2023-112 Code review fixes.
    
    
    Approved-by: Mateusz Lesiak
    Approved-by: Pawel.Tulinski

--- src/components/Organisms/ProductFilters/index.tsx
+++ src/components/Organisms/ProductFilters/index.tsx
@@ -1,9 +1,13 @@
 import React, { FunctionComponent } from 'react';
+import router from 'next/router';
 import useTranslation from 'next-translate/useTranslation';
 
+import CloseIcon from 'assets/icons/close.svg';
 import clsx from 'clsx';
+import Button from 'components/Atoms/Button';
 import ProductFilterItem from 'components/Molecules/ProductFilterItem';
 import ProductFilterTopBar from 'components/Molecules/ProductFilterTopBar';
+import { getSearchQuery } from 'utils/Hybris/endpoints';
 
 import styles from './ProductFilters.module.scss';
 
@@ -18,6 +22,10 @@ const TAG = 'ProductFilters';
 
 const ProductFilters: FunctionComponent<ProductFiltersProps> = ({ facets, breadcrumbs, visible, toggleVisible }) => {
   const { t } = useTranslation();
+  const resetFilters = () => {
+    const query = router.query;
+    router.push(getSearchQuery(`${query.query}`.split(':')[0]));
+  };
   return (
     <aside
       aria-label={t('components.productFilters.label')}
@@ -29,6 +37,14 @@ const ProductFilters: FunctionComponent<ProductFiltersProps> = ({ facets, breadc
             <ProductFilterItem key={facet.name} facet={facet} breadcrumbs={breadcrumbs} />
           ))}
         </ul>
+
+        <Button
+          onClick={resetFilters}
+          variant="button--with-icon"
+          className={styles['product-filters__reset-filters-button']}>
+          {t('components.productFilters.clearAllFilters')}
+          <CloseIcon />
+        </Button>
       </div>
     </aside>
   );
